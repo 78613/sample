d5d30d8c20752c9e8cee425336764e52663e2886
Omar Cardona <ocardona@us.ibm.com>
2014-04-02 13:51:28 -0500

    Modular sort layout

--- src/adts/adts_sort.c
+++ src/adts/adts_sort.c
@@ -43,6 +43,8 @@
  * \detail
  *    For sorting algorithms where small arrays have too much processing
  *    overhead, we create a threahold such as to perform a simpler sort.
+ *
+ *    It's best is this is set to processor cache line affinity
  ****************************************************************************
  */
 #define SORT_THREASHOLD (8)
@@ -437,8 +439,8 @@ sort_quick( int32_t arr[],
 
     int32_t j = sort_quick_partition(arr, lo, hi);
 
-    sort_quick(arr, lo, j - 1);
-    sort_quick(arr, j + 1, hi);
+    sort_quick(arr, lo, (j - 1));
+    sort_quick(arr, (j + 1), hi);
 
 exception:
     return;
@@ -561,152 +563,6 @@ adts_sort_quick_3way( int32_t arr[],
 ******************************************************************************/
 
 
-/*
- ****************************************************************************
- * test control
- *
- ****************************************************************************
- */
-static void
-utest_control( void )
-{
-
-    CDISPLAY("=========================================================");
-    {
-        CDISPLAY("Test: display ");
-        const int32_t arr[] = {1,9,2,8,3,7,4,6,5,0,99};
-        const size_t  elems = sizeof(arr) / sizeof(arr[0]);
-
-        adts_array_display(arr, elems);
-    }
-
-    CDISPLAY("=========================================================");
-    {
-        CDISPLAY("Test: unsorted array ");
-        const int32_t arr[] = {1,9,2,8,3,7,4,6,5,0,99};
-        const size_t  elems = sizeof(arr) / sizeof(arr[0]);
-        bool          rc    = false;
-
-        adts_array_display(arr, elems);
-        rc = adts_arr_is_sorted(arr, elems);
-        assert(false == rc);
-
-        rc = adts_arr_is_not_sorted(arr, elems);
-        assert(rc);
-    }
-
-    CDISPLAY("=========================================================");
-    {
-        CDISPLAY("Test: sorted array ");
-        const int32_t arr[] = {3,4,5,5,5,8,9,10,99};
-        const size_t  elems = sizeof(arr) / sizeof(arr[0]);
-        bool          rc    = false;
-
-        adts_array_display(arr, elems);
-        rc = adts_arr_is_sorted(arr, elems);
-        assert(rc);
-
-        rc = adts_arr_is_not_sorted(arr, elems);
-        assert(false == rc);
-    }
-
-    CDISPLAY("=========================================================");
-    {
-        CDISPLAY("Test: shuffle");
-        int32_t       rc    = 0;
-        int32_t       arr[] = {3,4,5,5,5,8,9,10,99};
-        const size_t  elems = sizeof(arr) / sizeof(arr[0]);
-
-        adts_array_display(arr, elems);
-        adts_shuffle(arr, elems);
-        printf("\n");
-        adts_array_display(arr, elems);
-        rc = adts_arr_is_not_sorted(arr, elems);
-        assert(rc);
-    }
-
-    CDISPLAY("=========================================================");
-    {
-        CDISPLAY("Test: quick sort insertion");
-        int32_t       rc    = 0;
-        int32_t       arr[] = {1,9,2,8,3,7,4,6,5,0,99};
-        const size_t  elems = sizeof(arr) / sizeof(arr[0]);
-
-        adts_array_display(arr, elems);
-        adts_sort_insertion(arr, elems);
-        printf("\n");
-        adts_array_display(arr, elems);
-        rc = adts_arr_is_sorted(arr, elems);
-        assert(rc);
-    }
-
-    CDISPLAY("=========================================================");
-    {
-        CDISPLAY("Test: shell sort");
-        int32_t       rc    = 0;
-        int32_t       arr[] = {1,9,2,8,3,7,4,6,5,0,99};
-        const size_t  elems = sizeof(arr) / sizeof(arr[0]);
-
-        adts_array_display(arr, elems);
-        adts_sort_shell(arr, elems);
-        printf("\n");
-        adts_array_display(arr, elems);
-        rc = adts_arr_is_sorted(arr, elems);
-        assert(rc);
-    }
-//FIXME:
-#if 0
-    CDISPLAY("=========================================================");
-    {
-        CDISPLAY("Test: merge sort");
-        int32_t       rc    = 0;
-        int32_t       arr[] = {1,9,2,8,3,7,4,6,5,0,99};
-        const size_t  elems = sizeof(arr) / sizeof(arr[0]);
-
-        adts_array_display(arr, elems);
-        rc = adts_sort_merge(arr, elems);
-        assert(0 == rc);
-        printf("\n");
-        adts_array_display(arr, elems);
-        rc = adts_arr_is_sorted(arr, elems);
-        assert(rc);
-    }
-
-    CDISPLAY("=========================================================");
-    {
-        CDISPLAY("Test: quick sort");
-        int32_t       rc    = 0;
-        int32_t       arr[] = {1,9,2,8,3,7,4,6,5,0,99};
-        const size_t  elems = sizeof(arr) / sizeof(arr[0]);
-
-        adts_array_display(arr, elems);
-        adts_sort_quick(arr, elems);
-        printf("\n");
-        adts_array_display(arr, elems);
-        rc = adts_arr_is_sorted(arr, elems);
-        assert(rc);
-    }
-
-    CDISPLAY("=========================================================");
-    {
-        CDISPLAY("Test: quick sort 3way");
-        int32_t       rc    = 0;
-        int32_t       arr[] = {1,9,2,8,3,7,4,6,5,0,99};
-        const size_t  elems = sizeof(arr) / sizeof(arr[0]);
-
-        adts_array_display(arr, elems);
-        adts_sort_quick_3way(arr, elems);
-        printf("\n");
-        adts_array_display(arr, elems);
-        rc = adts_arr_is_sorted(arr, elems);
-        //assert(rc);
-    }
-#endif
-
-    return;
-} /* utest_control() */
-
-
 
 /*
  ****************************************************************************
@@ -717,7 +573,7 @@ utest_control( void )
 void
 utest_adts_sort( void )
 {
-    utest_control();
+    utest_adts_sort_public();
 
     return;
 } /* utest_adts_sort() */
--- src/adts/adts_sort.h
+++ src/adts/adts_sort.h
@@ -7,39 +7,166 @@
 #include <inttypes.h>
 
 
-
-/**
- **************************************************************************
- * \details
- *   Prototypes
+/*!
+ * \brief Shuffle array elements.
  *
- **************************************************************************
+ * \param[in] arr[] - Input array.
+ * \param[in] elems - Elements in input array.
+ *
+ * \return none
+ *
+ * \note Time:  O(n)
+ * \note Space: O(1)
  */
 void
 adts_shuffle( int32_t arr[],
               size_t  elems );
+
+/*!
+ * \brief Determine is array is unsorted
+ *
+ * \param[in] arr[] - Input array.
+ * \param[in] elems - Elements in input array.
+ *
+ * \return true  - Input array is unsorted.
+ * \return false - Input array is sorted.
+ *
+ * \note Time:  O(n)
+ * \note Space: O(1)
+ */
 bool
 adts_arr_is_not_sorted( int32_t arr[],
                         size_t  elems );
+
+/*!
+ * \brief Determine is array is sorted
+ *
+ * \param[in] arr[] - Input array.
+ * \param[in] elems - Elements in input array.
+ *
+ * \return false - Input array is unsorted.
+ * \return true  - Input array is sorted.
+ *
+ * \note Time:  O(n)
+ * \note Space: O(1)
+ */
 bool
 adts_arr_is_sorted( int32_t arr[],
                     size_t  elems );
+
+/*!
+ * \brief Display array elements in ascending order.
+ *
+ * \param[in] arr[] - Input array.
+ * \param[in] elems - Elements in input array.
+ *
+ * \return void
+ *
+ * \note Time:  O(n)
+ * \note Space: O(1)
+ */
 void
 adts_array_display( int32_t       arr[],
                     const size_t  elems );
+
+/*!
+ * \brief Array sort using the shell algorithm.
+ *
+ * \param[in] arr[] - Input array.
+ * \param[in] elems - Elements in input array.
+ *
+ * \return void
+ *
+ * \note Time:      O(n^3/2) - h = 3 intrval, subject to change in the future
+ * \note Space:     O(1)
+ * \note Stability: no
+ */
 void
 adts_sort_shell( int32_t       arr[],
                  const size_t  elems );
+
+
+/*!
+ * \brief Extended array sort using the shell algorithm.
+ *
+ * This implementation allows for consumer selected upper and lower bounds.
+ *
+ * \param[in] arr[] - Input array.
+ * \param[in] elems - Elements in input array.
+ *
+ * \return void
+ *
+ * \note Time:      O(n^3/2) - h = 3 intrval, subject to change in the future
+ * \note Space:     O(1)
+ * \note Stability: no
+ */
 void
 adts_sort_shell_ext( int32_t arr[],
                      size_t  lo,
                      size_t  hi );
+
+/*!
+ * \brief Array sort using the insertion algorithm.
+ *
+ * \param[in] arr[] - Input array.
+ * \param[in] elems - Elements in input array.
+ *
+ * \return void
+ *
+ * \note Time:      O(n^2)
+ * \note Space:     O(1)
+ * \note Stability: yes
+ */
 void
 adts_sort_insertion( int32_t arr[],
                      size_t  elems );
+
+/*!
+ * \brief Extended array sort using the insertion algorithm.
+ *
+ * This implementation allows for consumer selected upper and lower bounds.
+ *
+ * \param[in] arr[] - Input array.
+ * \param[in] elems - Elements in input array.
+ *
+ * \return void
+ *
+ * \note Time:      O(n^2)
+ * \note Space:     O(1)
+ * \note Stability: yes
+ */
+void
+adts_sort_insertion_ext( int32_t arr[],
+                         size_t  lo,
+                         size_t  hi );
+/*!
+ * \brief Array sort using the merge algorithm.
+ *
+ * \param[in] arr[] - Input array.
+ * \param[in] elems - Elements in input array.
+ *
+ * \return void
+ *
+ * \note Time:      O(n log n)
+ * \note Space:     O(2n)
+ * \note Stability: yes
+ */
 int32_t
 adts_sort_merge( int32_t arr[],
                  size_t  elems );
+
+/*!
+ * \brief Array sort using the merge algorithm.
+ *
+ * \param[in] arr[] - Input array.
+ * \param[in] elems - Elements in input array.
+ *
+ * \return void
+ *
+ * \note Time:      O(n log n) - Average O(n^2) worst case very unlikely
+ * \note Space:     O(1)
+ * \note Stability: yes
+ */
 void
 adts_sort_quick( int32_t arr[],
                  size_t  elems );
@@ -55,6 +182,8 @@ adts_sort_quick( int32_t arr[],
  */
 void
 utest_adts_sort( void );
+void
+utest_adts_sort_public( void );
 
 
 #endif /* _H_ADTS_SORT */
