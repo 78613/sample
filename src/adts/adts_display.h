fe0ee55d7e93e492ec65d5e560ba56262281c4a4
Omar Cardona <omarcardona@yahoo.com>
2017-11-21 21:39:41 -0800

    add getpid() support to CDISPLAY()

--- src/adts/adts_display.h
+++ src/adts/adts_display.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <sched.h> /* sched_getcpu() */
-#include <stdio.h> /* printf() */
+#include <sched.h>     /* sched_getcpu() */
+#include <stdio.h>     /* printf() */
+#include <sys/types.h> /* getpid() */
 
 
 
@@ -17,17 +18,20 @@
  **************************************************************************
  */
 #if defined(__ADTS_DISPLAY)
-    #define CDISPLAY(_format, ...)                                         \
-    do {                                                                   \
-        char   _buffer[256] = {0};                                         \
-        size_t _limit       = sizeof(_buffer) - 1;                         \
-                                                                           \
-        snprintf(_buffer, _limit, _format, ## __VA_ARGS__);                \
-        printf("%3d %4d %-25.25s %-30.30s %s\n",                           \
-            sched_getcpu(), __LINE__, __FILE__, __FUNCTION__, _buffer);    \
-                                                                           \
-        /* Serialize console output on exit/error */                       \
-        fflush(stdout);                                                    \
+    #define CDISPLAY(_format, ...)                            \
+    do {                                                      \
+        char   _buffer[256] = {0};                            \
+        size_t _limit       = sizeof(_buffer) - 1;            \
+                                                              \
+        snprintf(_buffer, _limit, _format, ## __VA_ARGS__);   \
+        printf("%3u 0x%-8.8x %4u %-25.25s %-30.30s %s\n",     \
+               sched_getcpu(),                                \
+               getpid(),                                      \
+               __LINE__, __FILE__, __FUNCTION__,              \
+               _buffer);                                      \
+                                                              \
+        /* Serialize console output on exit/error */          \
+        fflush(stdout);                                       \
     } while(0);
 #else
     #define CDISPLAY(_format, ...) /* compile disabled */
