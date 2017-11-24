f9548b0008a9e2b9e8c0e50225a64df95201b4cc
Omar Cardona <omarcardona@yahoo.com>
2017-11-23 22:42:27 -0800

    add thread priority to CDISPLAY

--- src/adts/adts_display.h
+++ src/adts/adts_display.h
@@ -20,27 +20,33 @@
  **************************************************************************
  */
 #if defined(__ADTS_DISPLAY)
-    #define CDISPLAY(_format, ...)                                        \
-    do {                                                                  \
-        char       _buffer[256] = {0};                                    \
-        pid_t      _process     = getpid();                               \
-        size_t     _limit       = sizeof(_buffer) - 1;                    \
-        int32_t    _core        = sched_getcpu();                         \
-        int32_t    _processpri  = getpriority(PRIO_PROCESS, _process);    \
-        pthread_t  _thread      = pthread_self();                         \
-                                                                          \
-        snprintf(_buffer, _limit, _format, ## __VA_ARGS__);               \
-        printf("%3u 0x%-8.8x %2i 0x%-8.8x %s %5u %-25.25s %-30.30s %s\n", \
-               _core,                                                     \
-               _process,                                                  \
-               _processpri,                                               \
-               _thread,                                                   \
-               "|",                                                       \
-               __LINE__, __FILE__, __FUNCTION__,                          \
-               _buffer);                                                  \
-                                                                          \
-        /* Serialize console output on exit/error */                      \
-        fflush(stdout);                                                   \
+    #define CDISPLAY(_format, ...)                                             \
+    do {                                                                       \
+        char               _buffer[256] = {0};                                 \
+        pid_t              _process     = getpid();                            \
+        size_t             _limit       = sizeof(_buffer) - 1;                 \
+        int32_t            _core        = sched_getcpu();                      \
+        int32_t            _policy      = 0;                                   \
+        int32_t            _threadpri   = 0;                                   \
+        int32_t            _processpri  = getpriority(PRIO_PROCESS, _process); \
+        pthread_t          _thread      = pthread_self();                      \
+        struct sched_param _params      = {0};                                 \
+                                                                               \
+        _threadpri = pthread_getschedparam(_thread, &(_policy), &(_params));   \
+                                                                               \
+        snprintf(_buffer, _limit, _format, ## __VA_ARGS__);                    \
+        printf("%3u 0x%-8.8x %2i 0x%-8.8x %2i %s %5u %-25.25s %-30.30s %s\n",  \
+               _core,                                                          \
+               _process,                                                       \
+               _processpri,                                                    \
+               _thread,                                                        \
+               _threadpri,                                                     \
+               "|",                                                            \
+               __LINE__, __FILE__, __FUNCTION__,                               \
+               _buffer);                                                       \
+                                                                               \
+        /* Serialize console output on exit/error */                           \
+        fflush(stdout);                                                        \
     } while(0);
 #else
     #define CDISPLAY(_format, ...) /* compile disabled */
