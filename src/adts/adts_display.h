1970f7e45db1f77e0dd6429b5de429edc23fda2b
Omar Cardona <omarcardona@yahoo.com>
2017-11-23 22:32:31 -0800

    add process priority to CDISPLAY

--- src/adts/adts_display.h
+++ src/adts/adts_display.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <sched.h>     /* sched_getcpu() */
-#include <stdio.h>     /* printf() */
-#include <pthread.h>   /* pthread_self() */
-#include <sys/types.h> /* getpid() */
+#include <sched.h>        /* sched_getcpu() */
+#include <stdio.h>        /* printf() */
+#include <pthread.h>      /* pthread_self() */
+#include <sys/types.h>    /* getpid() */
+#include <sys/resource.h> /* getpriority() */
 
 
 
@@ -21,14 +22,19 @@
 #if defined(__ADTS_DISPLAY)
     #define CDISPLAY(_format, ...)                                        \
     do {                                                                  \
-        char   _buffer[256] = {0};                                        \
-        size_t _limit       = sizeof(_buffer) - 1;                        \
+        char       _buffer[256] = {0};                                    \
+        pid_t      _process     = getpid();                               \
+        size_t     _limit       = sizeof(_buffer) - 1;                    \
+        int32_t    _core        = sched_getcpu();                         \
+        int32_t    _processpri  = getpriority(PRIO_PROCESS, _process);    \
+        pthread_t  _thread      = pthread_self();                         \
                                                                           \
         snprintf(_buffer, _limit, _format, ## __VA_ARGS__);               \
-        printf("%3u 0x%-8.8x 0x%-8.8x %s %5u %-25.25s %-30.30s %s\n",     \
-               sched_getcpu(),                                            \
-               getpid(),                                                  \
-               pthread_self(),                                            \
+        printf("%3u 0x%-8.8x %2i 0x%-8.8x %s %5u %-25.25s %-30.30s %s\n", \
+               _core,                                                     \
+               _process,                                                  \
+               _processpri,                                               \
+               _thread,                                                   \
                "|",                                                       \
                __LINE__, __FILE__, __FUNCTION__,                          \
                _buffer);                                                  \
